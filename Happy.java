957afeaf2ade3e0fb70988245e03a46a67b1790e
kohalahal <52988648+kohalahal@users.noreply.github.com>
2019-08-19 21:29:50 +0900

    Create Happy.java

---
+++ Happy.java
@@ -0,0 +1,43 @@
+package Practice;
+import java.util.*;
+
+public class yay {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, I am Haeri Lee"); System.out.println("Hello"+"Bye");
+		int myage, thisyear, nextyear;
+		myage=29;
+		thisyear=2019;
+		nextyear=2020;
+		System.out.println("my age is "+myage);
+		System.out.println("this year is "+thisyear);
+		System.out.println("next year is "+nextyear);
+		
+		System.out.println("HAPPY NEW YEAR's EVE!!!");
+		System.out.println("my age is "+myage++);
+		System.out.println("this year is "+thisyear++);
+		System.out.println("next year is "+nextyear++);
+		System.out.println("HAPPY NEW YEAR!!!");
+		System.out.println("my age is "+myage);
+		System.out.println("this year is "+thisyear);
+		System.out.println("next year is "+nextyear);
+		
+		System.out.println("Hello?");
+		System.out.println("My Korean age is "+myage+2);
+		System.out.println("oops, i am not that old yet");
+		int koreanage;
+		koreanage=myage+2;
+		System.out.println("again, my Korean age is "+koreanage);
+		float age=29.6f;
+		System.out.println("\"my accurate age is \""+age);
+		final String MYNAME="haeri";
+		char a='a';
+		long b=9999999999999l;
+		double c=-0.1234556;	
+		System.out.println(MYNAME);
+		
+
+		
+	}
+	
+}
