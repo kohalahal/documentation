0e31d33d5c4ca6954b7a00ebb70773810a6ac080
kohalahal <52988648+kohalahal@users.noreply.github.com>
2019-08-19 21:25:26 +0900

    Create number.java

---
+++ number.java
@@ -0,0 +1,27 @@
+package Practice;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b = 2;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		int c=5;
+		System.out.println(c);
+		System.out.println(c+a);
+		int tmp;
+		tmp=a;
+		a=b;
+		b=tmp;
+		System.out.println("a="+a);
+		System.out.println("b="+b);// TODO Auto-generated method stub
+		final int ME=31;
+		System.out.println(ME+"살");
+		long regNo = 8911222012345l;
+		System.out.println('1'+2);
+		System.out.println('1'+'2');
+
+	}
+
+}
